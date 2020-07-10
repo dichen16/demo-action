e400befa75226c272e7c4ec4c639a73dbd000cb1
Di Chen <dichen@redhat.com>
2020-07-11 02:19:59 +0800

    show db version

--- scripts/conn.py
+++ scripts/conn.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 try:
-    conn = psycopg2.connect(host="localhost",database="mydb", user="test", password="test")
+    conn = psycopg2.connect(host="localhost",database="mydb", user="test", password="test", port='5432')
     print("connected!!!")
 except:
     print("I am unable to connect to the database")
