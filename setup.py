21a9bec4f992c7f4b2287662b6fbadc3a7c14c20
Di Chen <dichen@redhat.com>
2020-07-26 21:22:10 +0800

    package never use dash

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='demo-action',
+    name='demoaction',
     version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
